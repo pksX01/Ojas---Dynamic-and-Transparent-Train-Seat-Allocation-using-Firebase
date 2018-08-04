5943cd098e467fe9a317a100cecc23d0c40b1484
pksX01 <pksx01@gmail.com>
2018-08-04 20:18:53 +0530

    initial commit

---
+++ app/src/main/java/com/example/rudra/ojas_for_tte/Sign_in.java
@@ -0,0 +1,13 @@
+package com.example.rudra.ojas_for_tte;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Sign_in extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_in);
+    }
+}
